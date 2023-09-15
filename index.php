e449eb96ba2aee75f46d00db6fc8ed518eef9fd9
Topandhi <85047888+Topandhi@users.noreply.github.com>
2023-09-15 10:32:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
    </head>
-<body style="background-image: url('My_Heart_in_the_Water.png');background-position: top;background-repeat: no-repeat;padding:0px;">
+<body style="background-image: url('My_Heart_in_the_Water.png');background-position: top;background-repeat: repeat;padding:0px;">
     <div style="display:flex;flex-direction:row;justify-content:center;">
         <divmeter data-height="200px" data-time="2014-01-11T01:12:59.371Z" data-type="static" id="divmeter-1672461300374" class="divmeter divmeter-complete"><img id="i0" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="in" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/11.jpg" style="height: 200px;"><img id="i1" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="i2" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="i3" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="i4" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="i5" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"><img id="i6" src="https://raw.github.com/dogancelik/divergence-meter/gh-pages/img/0.jpg" style="height: 200px;"></divmeter>
     </div>
@@ -31,55 +31,55 @@
             const imgElement6 = document.querySelector('#i6');
 
             function change() {
-            <?php
-            date_default_timezone_set('Asia/Jakarta');
-            // Get the current date and time
-            $currentDateTime = new DateTime();
+            // Set the time zone (JavaScript uses the browser's time zone)
+            // You may want to convert time zones if necessary
+            // For example: 'Asia/Jakarta' to 'America/New_York'
+            // Please note that JavaScript doesn't have built-in support for time zones like PHP
+            // You might need to use external libraries like "moment-timezone" for more precise time zone handling
+            // Example: https://momentjs.com/timezone/
+            const timeZone = 'Asia/Jakarta';
+
+            // Get the current date and time in the specified time zone
+            const currentDateTime = new Date();
+            currentDateTime.toLocaleString('en-US', { timeZone });
 
             // Define the specific date you want to calculate the interval to
-            $specificDate = new DateTime('2024-04-10 08:00:00'); // Change this to your specific date and time
+            const specificDate = new Date('2024-04-10T08:00:00');
 
-            // Calculate the interval
-            $interval = $currentDateTime->diff($specificDate);
+            // Calculate the interval in milliseconds
+            const interval = specificDate - currentDateTime;
 
-            // Extract days, hours, minutes, and seconds
-            $days = $interval->format('%a');
-            $hours = $interval->format('%h');
-            $minutes = $interval->format('%i');
-            $minutes = (int)($minutes/10);
-            $seconds = $interval->format('%s');
-                $length = 4;
-                // Calculate the number of days in the given interval
-                $days = abs($days);
-            
-                // Convert the number of days to a string
-                $daysStr = strval($days);
-            
-                // Calculate the number of zeros to pad
-                $padLength = max(0, $length - strlen($daysStr));
-            
-                // Create an array of zeros to pad
-                $zeroArray = array_fill(0, $padLength, 0);
-            
-                // Split the string into an array of digits
-                $digitArray = str_split($daysStr);
-            
-                // Convert the string digits back to integers
-                $digitArray = array_map('intval', $digitArray);
-            
-                // Combine the zero padding and digit array
-                $r = array_merge($zeroArray, $digitArray);
-                // var_dump($r);
-            ?>
+            // Calculate days, hours, minutes, and seconds
+            const days = Math.floor(interval / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((interval % (1000 * 60)) / 1000);
+
+            // Format the result as needed
+            const formattedDays = days.toString().padStart(4, '0');
+            const formattedHours = hours.toString().padStart(2, '0');
+            const formattedMinutes = Math.floor(minutes / 10);
+            const formattedSeconds = seconds.toString().padStart(2, '0');
+
+            // Output the formatted result
+            console.log(`Days: ${formattedDays}, Hours: ${formattedHours}, Minutes: ${formattedMinutes}, Seconds: ${formattedSeconds}`);
             var min = 0;
             var max = images.length-1;
-            var r = <?= $r[0]; ?>;
-            var r1 = <?= $r[1]; ?>;
-            var r2 = <?= $r[2]; ?>;
-            var r3 = <?= $r[3]; ?>;
-            var r4 = <?= $hours[0]; ?>;
-            var r5 = <?= $hours[1]; ?>;
-            var r6 = <?= $minutes ?>;
+            // var r = <?= $r[0]; ?>;
+            // var r1 = <?= $r[1]; ?>;
+            // var r2 = <?= $r[2]; ?>;
+            // var r3 = <?= $r[3]; ?>;
+            // var r4 = <?= $hours[0]; ?>;
+            // var r5 = <?= $hours[1]; ?>;
+            // var r6 = <?= $minutes ?>;
+            
+            var r = formattedDays[0];
+            var r1 = formattedDays[1];
+            var r2 = formattedDays[2];
+            var r3 = formattedDays[3];
+            var r4 = formattedHours[0];
+            var r5 = formattedHours[1];
+            var r6 = formattedMinutes;
                 // console.log('imgElement',imgElement,r,r1,r2,r3,r4,r5,r6);
                 imgElement.src = images[r];
                 imgElement1.src = images[r1];
